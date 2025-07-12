d142b10e6a8627a7aad5f331415f3d8805ba88b9
Dice78 <georgyjes@gmail.com>
2025-07-12 20:19:35 +0200

    Kretanje i collision
    Ubacio 5 test tajlova
    Napravio collision sa ljubicastim tajlom
    Napravio test mapu i point of interest

--- Scripts/CharacterScripts/CharacterMovement.gd
+++ Scripts/CharacterScripts/CharacterMovement.gd
@@ -1,24 +1,35 @@
 extends CharacterBody2D
 
 const Speed: int = 200
-#moguci unosi
+@onready var ray = $RayCast2D  # Make sure this RayCast2D node exists in the scene
+
+# Directional inputs
 var Inputs = {
 	"right": Vector2.RIGHT,
 	"left": Vector2.LEFT,
 	"up": Vector2.UP,
 	"down": Vector2.DOWN
 }
-#snapuje lika na jedno od polja
+
+# Snap character to grid center
 func SnapCharacterToGrid():
-	position = position.snapped(Vector2.ONE * $"../Grid".tile_set.tile_size.x) + Vector2.ONE * $"../Grid".tile_set.tile_size.x / 2
+	var tile_size = $"../Grid".tile_set.tile_size.x
+	position = position.snapped(Vector2.ONE * tile_size) + Vector2.ONE * tile_size / 2
+
+# Movement with collision check via RayCast2D
 func Move(Direction):
-	var TargetPosition = position + Inputs[Direction] * $"../Grid".tile_set.tile_size.x
-	position = TargetPosition
-#pokrece se jednom kada se upali skripta
+	var tile_size = $"../Grid".tile_set.tile_size.x
+	ray.target_position = Inputs[Direction] * tile_size  # Aim ray in direction of movement
+	ray.force_raycast_update()
+
+	if !ray.is_colliding():
+		position += Inputs[Direction] * tile_size
+
+# Called once when the scene is ready
 func _ready():
 	SnapCharacterToGrid()
-##kada se pritisne dugme, provjerava da li je neko od dugmica za kretanje pritisnuto
-##ako jeste, pozove se metoda move koja pomjera karaktera na to polje
+
+# Handle input (arrow keys or WASD, if mapped in project settings)
 func _unhandled_input(event: InputEvent):
 	for Direction in Inputs.keys():
 		if event.is_action_pressed(Direction):
