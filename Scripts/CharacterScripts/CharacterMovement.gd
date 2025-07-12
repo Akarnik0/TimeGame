75bc5ec74b52a643023b243dba42dea2988a6d8a
Akarnik0 <brestovacnikola5@gmail.com>
2025-07-13 01:19:55 +0200

    dodata provjera prolaza kroz vrata

--- Scripts/CharacterScripts/CharacterMovement.gd
+++ Scripts/CharacterScripts/CharacterMovement.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
-
-const Speed: int = 200
+#varijabla u kojoj ce se cuvaju podaci o lokacijama svih vrata unutar jsona
+var AllDoors = {}
+#varijabla u kojoj se cuvaju podaci o lokacijama vrata u levelu unutar jsona
+var Doors
 @onready var ray = $RayCast2D  # Make sure this RayCast2D node exists in the scene
+
+var IsInDoor: bool
+var SameRoomPosition
 
 # Directional inputs
 var Inputs = {
@@ -10,7 +15,20 @@ var Inputs = {
 	"up": Vector2.UP,
 	"down": Vector2.DOWN
 }
-
+#Dobije podatke o lokaciji svih vrata unutar jsona
+func GetDoorData(FilePath: String):
+	var File = FileAccess.open(FilePath, FileAccess.READ)
+	var JsonText = File.get_as_text()
+	var ResultText = JSON.parse_string(JsonText)
+	AllDoors = ResultText
+#Dobije podatke o lokaciji vrata iz levela unutar jsona
+func GetDoorDataForLevel(LevelName: String):
+	var RawArray = AllDoors.get(LevelName,[])
+	var LevelDoors = []
+	for Pair in RawArray:
+		if Pair.size() == 2:
+			LevelDoors.append(Vector2(Pair[0], Pair[1]))
+	return LevelDoors
 # Snap character to grid center
 func SnapCharacterToGrid():
 	var tile_size = $"../Grid".tile_set.tile_size.x
@@ -24,10 +42,25 @@ func Move(Direction):
 
 	if !ray.is_colliding():
 		position += Inputs[Direction] * tile_size
+		var LastPosition = position
+		if position != SameRoomPosition && IsInDoor == true:
+			GlobalLevel.Turns += 1
+			print(GlobalLevel.Turns)
+			if GlobalLevel.Turns >=12:
+				print("gotovo")
+			IsInDoor = false
+		else:
+			IsInDoor = false
+		for i in Doors:
+			if position == i:
+				IsInDoor = true
+				SameRoomPosition = LastPosition
 
 # Called once when the scene is ready
 func _ready():
 	SnapCharacterToGrid()
+	GetDoorData("res://JSON/DoorLocations.json")
+	Doors = GetDoorDataForLevel(GlobalLevel.LevelName)
 
 # Handle input (arrow keys or WASD, if mapped in project settings)
 func _unhandled_input(event: InputEvent):
---
+++ Scripts/LevelScripts/LevelName.gd
@@ -0,0 +1,6 @@
+extends Node2D
+var LevelName: String
+var Turns: int
+func _ready():
+	LevelName = "Level1"
+	Turns = 0
