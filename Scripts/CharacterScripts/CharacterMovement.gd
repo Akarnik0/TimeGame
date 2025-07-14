86ade72d32ed6405db96d13b4c6749853385fa07
Akarnik0 <brestovacnikola5@gmail.com>
2025-07-14 21:49:07 +0200

    Napravljeno osnovno vracanje vremena

--- Scripts/CharacterScripts/CharacterMovement.gd
+++ Scripts/CharacterScripts/CharacterMovement.gd
@@ -16,10 +16,31 @@ var Inputs = {
 	"up": Vector2.UP,
 	"down": Vector2.DOWN
 }
+#svi koraci koje je obavio karakter
+var Character_movements: Array[Vector2] = []
+#timer za vracanje unazad,varijabla za provjeru vracanja unazad,broj clana niza
+var Is_going_back = false
+var Going_back_id = 0
+var Going_back_timer := Timer.new()
+func Start_going_back():
+	if Character_movements.size() > 1:
+		Is_going_back = true
+		Going_back_id = Character_movements.size() - 2
+		Going_back_timer.start()
+func _On_go_back_step():
+	if Going_back_id >= 0:
+		position = Character_movements[Going_back_id]
+		if(Going_back_timer.wait_time >= 0.05):
+			Going_back_timer.wait_time -= 0.01
+		Going_back_id -= 1
+	else:
+		Going_back_timer.stop()
+		Is_going_back = false
 # Snap character to grid center
 func SnapCharacterToGrid():
 	var tile_size = $"../Grid".tile_set.tile_size.x
 	position = position.snapped(Vector2.ONE * tile_size) + Vector2.ONE * tile_size / 2
+	Character_movements.append(position)
 
 # Movement with collision check via RayCast2D
 func Move(Direction):
@@ -31,11 +52,13 @@ func Move(Direction):
 	if !ray.is_colliding():
 		var LastPosition = position
 		position += Inputs[Direction] * tile_size
+		Character_movements.append(position)
 		if position != SameRoomPosition && IsInDoor == true:
 			GlobalLevel.Turns += 1
 			print("Turns: ",GlobalLevel.Turns)
 			if GlobalLevel.Turns >=12:
 				print("gotovo")
+				Start_going_back()
 			IsInDoor = false
 		else:
 			IsInDoor = false
@@ -49,21 +72,30 @@ func _ready():
 	SnapCharacterToGrid()
 	GetDoorData("res://JSON/DoorLocations.json")
 	Doors = GetDoorDataForLevel(GlobalLevel.LevelName)
-
+	
 	$InteractArea.connect("area_entered", Callable(self, "_on_InteractArea_area_entered"))
 	$InteractArea.connect("area_exited", Callable(self, "_on_InteractArea_area_exited"))
+	#timer za kada se bude vracalo vrijeme unazad
+	Going_back_timer.wait_time = 0.2
+	Going_back_timer.one_shot = false
+	Going_back_timer.connect("timeout", Callable(self, "_On_go_back_step"))
+	add_child(Going_back_timer)
 
 # Handle input (arrow keys or WASD, if mapped in project settings)
 func _unhandled_input(event: InputEvent):
-	for Direction in Inputs.keys():
-		if event.is_action_pressed(Direction):
-			Move(Direction)
-
-	if event.is_action_pressed("interact"):
-		for obj in interactables:
-			if obj.has_method("activate"):
-				obj.activate(self)
-				break
+	#sprijecava da se nesto unese dok se vraca vrijeme
+	if Is_going_back:
+		print("test")
+		return
+	else:
+		for Direction in Inputs.keys():
+			if event.is_action_pressed(Direction):
+				Move(Direction)
+		if event.is_action_pressed("interact"):
+			for obj in interactables:
+				if obj.has_method("activate"):
+					obj.activate(self)
+					break
 
 
 
