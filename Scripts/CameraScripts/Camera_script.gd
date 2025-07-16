c7bc27f809d8ba7404378884289c384772d24a2e
Akarnik0 <brestovacnikola5@gmail.com>
2025-07-17 00:25:39 +0200

    Popravljen bug pri pomjeranju kamere

--- Scripts/CameraScripts/Camera_script.gd
+++ Scripts/CameraScripts/Camera_script.gd
@@ -4,6 +4,7 @@ extends Camera2D
 var player
 #za provjerivanje stanja kamere
 var Is_on_character: bool
+var Is_moving_camera: bool
 
 func Get_level_center():
 	var rect: Rect2i = $"../Grid".get_used_rect()
@@ -12,28 +13,33 @@ func Get_level_center():
 	return center_local
 #pomjeri kameru u centar mape
 func Move_camera_to_center(center: Vector2):
+	Is_moving_camera = true
 	var tween = get_tree().create_tween()
 	tween.tween_property($".", "zoom", Vector2(1, 1), 1.0).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property($".", "global_position", Vector2(center), 1.0).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	tween.chain().tween_callback(Callable(self, "_on_move_to_center"))
 func _on_move_to_center():
 	Is_on_character = false
+	Is_moving_camera = false
 #pomjeri kameru na karaktera
 func Move_camera_to_character(character: Vector2):
+	Is_moving_camera = true
 	var tween = get_tree().create_tween()
 	tween.tween_property($".", "global_position", Vector2(character), 1.0).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property($".", "zoom", Vector2(3, 3), 1.0).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
 	tween.chain().tween_callback(Callable(self, "_on_move_to_character"))
 func _on_move_to_character():
 	Is_on_character = true
+	Is_moving_camera = false
 func _ready():
 	player = get_node("/root/Level/Character")
 	Is_on_character = true
+	Is_moving_camera = false
 func _process(_delta):
 	if Is_on_character == true:
 		position = player.position
 func _unhandled_input(event: InputEvent):
-	if event.is_action_pressed("show_map") && Is_on_character == true:
+	if event.is_action_pressed("show_map") && Is_on_character == true && Is_moving_camera == false:
 		Move_camera_to_center(Get_level_center())
-	elif event.is_action_pressed("show_map") && Is_on_character == false:
+	elif event.is_action_pressed("show_map") && Is_on_character == false && Is_moving_camera == false:
 		Move_camera_to_character(player.position)
